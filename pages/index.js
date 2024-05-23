9ffda1a2c9ed9b10e1e273985a78ebf4ac6f999f
Maicon Rodrigues <maiconrodriguessc@gmail.com>
2024-05-23 23:16:56 +0000

    edit message

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Thaynara bbzinha, I love you! If you love me give me some giggle!</h1>
+    return <h1>Thaynara bbzinha, I love you! If you love me give me some giggle! 😎</h1>
 }
 
 export default Home;
