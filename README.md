ae235d78fce372935f3bc35b58610347fd91d9a3
Maicon Rodrigues <maiconrodriguessc@gmail.com>
2024-10-11 22:27:08 +0000

    add scripts `lint:check` and `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Project created to implement tabnews.com.br for our dearest curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Thaynara bbzinha, I love you! If you love me give me some giggle! 😎</h1>
+  return (
+    <h1>
+      Thaynara bbzinha, I love you! If you love me give me
+      some giggle! 😎
+    </h1>
+  );
 }
 
 export default Home;
